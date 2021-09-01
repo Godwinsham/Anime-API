76d41a236e5cff4c408e4c84addee0a224462d87
godwinsham <godwinelijah0102@gmail.com>
2021-09-01 23:25:02 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const element = document.createElement('div');
 element.setAttribute('id','Div1');
 document.body.appendChild(element);
-element.innerHTML ='<form class="searchbar" action=" ">  <input type="text" placeholder="Search.." name="search"><button type="submit"><i class="fa fa-search"></i></button></input></form>';
+element.innerHTML ='<form id="form1" class="searchbar">  <input type="text" placeholder="Search.." name="search" required><button type="submit"><i class="fa fa-search"></i></button></input></form>';
 
+const form =document.getElementById('form1');
+form.addEventListener('submit',async(e)=>{
+    e.preventDefault();
+    const animeName = e.target.search.value
+    const url = `https://api.jikan.moe/v3/search/anime?q=${animeName}`
+    let response;
+   
+    try {
+      response = await fetch(url).then((res) => res.json());
+    } catch (error) {
+        alert("Something went wrong / Enter a valid Anime Name")
+    }
+    const animeData = response.results;
+    console.log(animeData);
+    }
+    
+ );
+ 
+
+    
 
 
 
