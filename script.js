f5fa77aa8c710b6ff5accb049e4a607a417e31a2
godwinsham <godwinelijah0102@gmail.com>
2021-09-01 18:13:32 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const element = document.createElement('div');
+element.setAttribute('id','Div1');
+document.body.appendChild(element);
+element.innerHTML ='<form class="searchbar" action=" ">  <input type="text" placeholder="Search.." name="search"><button type="submit"><i class="fa fa-search"></i></button></input></form>';
+
+
+
+
